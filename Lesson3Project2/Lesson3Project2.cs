7ee04e2d73b6199ccfb7287c782a96df27dade10
Sergey Lesikov <sergey_lesikov@mail.ru>
2021-02-22 18:58:44 +0300

    Lesson3

---
+++ Lesson3Project2/Lesson3Project2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lesson3Project2
+{
+    class Lesson3Project2
+    {
+        static void Main(string[] args)
+        {
+            string[][] mailCatalog = new string[][] {
+                new string[]{ "test0", "test0@mail.ru"},
+                new string[]{ "test1", "test1@mail.ru"},
+                new string[]{ "test2", "test2@mail.ru"},
+                new string[]{ "test3", "test3@mail.ru"},
+                new string[]{ "test4", "test4@mail.ru"},
+                new string[]{ "test5", "test5@mail.ru"},
+                new string[]{ "test6", "test6@mail.ru"},
+                new string[]{ "test7", "test7@mail.ru"},
+                new string[]{ "test8", "test8@mail.ru"},
+                new string[]{ "test9", "test9@mail.ru"}
+            };
+
+            Console.WriteLine("Справочник");
+
+            for (int i = 0; i < mailCatalog.Length; Console.WriteLine($"name: {mailCatalog[i][0]}; email: {mailCatalog[i++][0]};"));
+
+            Console.WriteLine("Нажмите на любую кнопку для выхода из программы.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson3Project5/Lesson3Project5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lesson3Project5
+{
+    class Lesson3Project5
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длинну массива: ");
+            int len = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[len];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"Введите {i} элемент массива: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            Console.Write("Введите смещение массива: ");
+            int offset = Convert.ToInt32(Console.ReadLine());
+
+            //bool oLOR = offset > 0; 
+
+            for (int i = offset > 0 ? arr.Length - 1 - offset : -offset; offset > 0 ? i >= 0 : i < arr.Length; i += offset > 0 ? -1 : 1)
+            {
+                arr[i + offset] = arr[i];
+                arr[i] = 0;
+            }
+
+            Console.WriteLine("Вывод массива со смещением: ");
+
+            for (int i = 0; i < arr.Length; Console.WriteLine($"{i} элемент массива: {arr[i++]}"));
+
+            Console.WriteLine("Нажмите на любую кнопку для выхода из программы.");
+            Console.ReadKey();
+        }
+    }
+}
