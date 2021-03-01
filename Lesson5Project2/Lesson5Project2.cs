3c1bde63b329144174b3d55dac1b4392ce177ed1
Sergey Lesikov <sergey_lesikov@mail.ru>
2021-03-01 17:45:39 +0300

    add Project 1, 2

---
+++ Lesson5Project2/Lesson5Project2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson5Project2
+{
+    class Lesson5Project2
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "startup.txt";
+
+            File.AppendAllText(fileName, $"{DateTime.Now}\n");
+
+            Console.WriteLine($"Текущие дата и время дописаны в файл \"{fileName}\"");
+
+            Console.WriteLine("Нажмите на любую кнопку для выхода из программы.");
+            Console.ReadKey();
+        }
+    }
+}
