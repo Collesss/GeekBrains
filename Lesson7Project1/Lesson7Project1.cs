f5b48ce27e5e66cce8ed4c3e9be30ed114a889e6
Sergey Lesikov <sergey_lesikov@mail.ru>
2021-03-21 22:18:12 +0300

    add Project 1 empty

---
+++ Lesson7Project1/Lesson7Project1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson7Project1
+{
+    class Lesson7Project1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
