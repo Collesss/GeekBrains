822e7b46a4970d507cfc355b4e24299cbd6634e8
Sergey Lesikov <sergey_lesikov@mail.ru>
2021-03-22 17:52:13 +0300

    update Project 1

---
+++ Lesson7Project1/TicTacToe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson7Project1
+{
+    class TicTacToe
+    {
+
+        public TicTacToe(int sizeX, int sizeY, int win)
+        {
+            if (sizeX <= 0 || sizeY <= 0 || win <= 0 || (win > sizeX && win > sizeY))
+                throw new ArgumentException("ti eblan prover argumenti libo choto menishe 0 libo nikogda ne bydet ");
+
+        }
+
+    }
+}
